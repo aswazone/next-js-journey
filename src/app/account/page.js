43cc292c30b1218687a14af24324700ed6f5f606
aswazone <aswamanu143@gmail.com>
2025-04-06 13:03:15 +0530

    feat(home):Navigations by -> useRouter, Link and redirect in (server Comp)

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -1,4 +1,10 @@
+import { redirect } from "next/navigation";
+
 export default function Accounts(){
+        //make a conditonal redirect
+        let userProfileInfo = null;
+        if(!userProfileInfo) redirect('profile')
+            
     return <div className="p-3 bg-teal-800">
         <h1>Accounts page</h1>
     </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,28 @@
+'use client'
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 
 //index route
 export default function Home() {
 
+  const router = useRouter();
+
+  const handleNavigate = () =>{
+    router.push('product')
+  }
+
   return (
     <center className="p-4">
       <h1 className="text-3xl">Welcome to Next JS</h1>
-      
+      <div className="flex justify-center gap-3">
+        <Link href={'product'}>Navigate to Products</Link>
+        <Link href={'account'}>Navigate to Accounts</Link>
+      </div>
+      <div className="mt-4">
+        <h1 className="text-lg font-bold">Alternative way of doing Navigate - useRouter </h1>
+        <button onClick={handleNavigate} className="btn bg-amber-500 p-2 rounded hover:bg-amber-400">Navigate to Products page</button>
+      </div>
     </center>
   );
 }
